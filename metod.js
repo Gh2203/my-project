7add31baa9cfd050b757d6d235b5bd10af94703c
Gor <hovhannisyangor374@gmail.ru>
2021-03-31 20:12:10 +0400

    metod.js

---
+++ metod.js
@@ -0,0 +1,83 @@
+// let str = "hovhannisyan";
+
+// console.log(str.toUpperCase());
+
+
+// let str = "HoVhAnNiSyAn";
+// console.log(str.toLowerCase());
+
+
+// let g = 3.56754;
+// console.log( g.toFixed(2));
+
+
+// let num = Number("768"); 
+// console.log(num);
+
+
+// let x = 3.6e6;
+// console.log(x);
+
+
+// let x = 50;
+// console.log(x.toString(2));
+
+
+// let y = 2.8;
+// console.log(Math.round(y));
+
+
+// let h = 4.6785;
+// console.log(Math.ceil(h*10)/10);
+
+
+// console.log(isNaN("str"));
+
+
+// console.log(isFinite("gor"));
+
+
+// console.log(parseInt('108languages'));
+
+
+//  console.log(Math.floor( Math.random()*10 ));
+
+
+// console.log(`my\nm`.length);
+
+
+// let str = 'Gor';
+
+// console.log(str[0]);
+
+
+// for (let char of "Hovhannisyan") {
+
+// console.log(char);
+// }
+
+
+// let str = 'gor wkth id';
+
+// console.log( str.indexOf("id" ));
+
+
+// console.log( "a".codePointAt(0) );
+// console.log( "Z".codePointAt(0) );
+
+
+// let arr = ["its", "a", "car"];
+// delete arr[2];
+// console.log (arr);
+
+
+// let arr = ["Я", "изучаю", "JavaScript"];
+
+
+// arr.splice(2, 1, "сложный", "язык");
+
+// console.log( arr ); 
+
+
+let arr = ["gor"];
+console.log( arr.concat(["hovhannisyan"]));
